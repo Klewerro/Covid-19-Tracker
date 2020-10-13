b8dfd139f03aa6bbc78bbd4f09f1dab3dcc6dbe0
PSzymon <pol.szym95@gmail.com>
2020-10-14 00:04:36 +0100

    Navigation component setted up

--- app/src/main/java/com/klewerro/covidapp/MainActivity.kt
+++ app/src/main/java/com/klewerro/covidapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.klewerro.covidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navigation_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
     }
 }
---
+++ app/src/main/java/com/klewerro/covidapp/ui/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.klewerro.covidapp.ui
+
+import androidx.fragment.app.Fragment
+import com.klewerro.covidapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
