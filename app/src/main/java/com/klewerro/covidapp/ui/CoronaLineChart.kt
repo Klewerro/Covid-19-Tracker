29ca8f3c67be3e68ffae1ec62fcf9f3adde34c44
PSzymon <pol.szym95@gmail.com>
2020-11-05 23:24:27 +0100

    Chart: CoronaLineChart class created, to encapsulate whole graph creation process.

---
+++ app/src/main/java/com/klewerro/covidapp/ui/CoronaLineChart.kt
@@ -0,0 +1,93 @@
+package com.klewerro.covidapp.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.klewerro.covidapp.R
+import com.klewerro.covidapp.model.CountryData
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+class CoronaLineChart(context: Context, attrs: AttributeSet) : LineChart(context, attrs) {
+
+    init {
+        //data = lineData
+        description.isEnabled = false
+        legend.isEnabled = false
+        xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
+        xAxis.setDrawGridLines(false)
+        axisLeft.isEnabled = false
+        axisRight.yOffset = -6f
+        axisRight.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+        //xAxis.granularity = 10f
+        isDoubleTapToZoomEnabled = false
+        setDrawGridBackground(false)
+        //setVisibleXRangeMinimum(6f)
+        axisLeft.axisMinimum = 0f
+        axisRight.axisMinimum = 0f
+        axisRight.isDrawBottomYLabelEntryEnabled
+
+        // set onClick marker
+        setupOnClickMarker()
+    }
+
+    fun setLineDataSet(timelineDataList: List<CountryData.TimelineData>) {
+        addIdToTimelineData(timelineDataList)
+        val entries = timelineDataList.map { Entry(it.id.toFloat(), it.confirmed.toFloat()) }
+        val lineDataSet1 = LineDataSet(entries, "Confirmed cases")  // it.confirmed
+        lineDataSet1.setDrawFilled(true)
+        lineDataSet1.fillDrawable = ContextCompat.getDrawable(context, R.drawable.graph_line_gradient)
+        lineDataSet1.color = ContextCompat.getColor(context, R.color.design_default_color_primary)
+        lineDataSet1.circleHoleColor = ContextCompat.getColor(context, R.color.white)
+        lineDataSet1.setCircleColor(R.color.design_default_color_primary)
+        lineDataSet1.lineWidth = 4f
+        lineDataSet1.highLightColor = ContextCompat.getColor(context, R.color.design_default_color_primary)
+        lineDataSet1.setDrawHorizontalHighlightIndicator(false)
+        lineDataSet1.setDrawValues(false)
+
+        val lineData = LineData(lineDataSet1)
+        val dates = timelineDataList.map { it.date }
+
+        this.data = lineData
+        this.xAxis.valueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                val date = dates[value.roundToInt()]
+                val formatter = SimpleDateFormat("dd.MM")
+                return formatter.format(date)
+            }
+        }
+
+        this.axisRight.valueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                if (value == 0f)
+                    return ""
+
+                return NumberFormat.getNumberInstance(Locale.US).format(value)
+            }
+        }
+
+        invalidate()
+    }
+
+
+    private fun setupOnClickMarker() {
+        val mv = CustomMarkerView(context, R.layout.custom_marker_view)
+        mv.chartView = this
+        this.marker = mv
+    }
+
+    private fun addIdToTimelineData(timelineData: List<CountryData.TimelineData>) {
+        for ((i, data) in timelineData.withIndex()) {
+            data.id = i
+        }
+    }
+}
--- app/src/main/java/com/klewerro/covidapp/ui/HomeFragment.kt
+++ app/src/main/java/com/klewerro/covidapp/ui/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.klewerro.covidapp.ui
 
 import android.os.Bundle
-import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
@@ -14,8 +13,6 @@ import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.highlight.Highlight
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.github.mikephil.charting.utils.MPPointD
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -42,8 +39,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             confirmedTextView.text = "Confirmed: ${countryData.todayStatistic.confirmed}"
             deathsTextView.text = "Deaths: ${countryData.todayStatistic.deaths}"
 
-            prepareChart(countryData.timelineData)
-            prepareDatePicker()
+            chart.setLineDataSet(countryData.timelineData.reversed())
 
             countryTextView.setOnClickListener {
                 datePicker.show(requireActivity().supportFragmentManager, "DATE_PICKER")
@@ -59,57 +55,5 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             }
         }
     }
-
-
-    private fun prepareChart(timelineData: List<CountryData.TimelineData>) {
-        val timelineDateReversed = timelineData.reversed()
-        addIdToTimelineData(timelineDateReversed)
-        val entries = timelineDateReversed.map { Entry(it.id.toFloat(), it.confirmed.toFloat())}
-
-        val lineDataSet1 = LineDataSet(entries, "Confirmed cases")  // it.confirmed
-        val lineData = LineData(lineDataSet1)
-        lineDataSet1.setDrawFilled(true)
-        lineDataSet1.fillDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.graph_line_gradient)
-        lineDataSet1.color = ContextCompat.getColor(requireContext(), R.color.design_default_color_primary)
-        lineDataSet1.circleHoleColor = ContextCompat.getColor(requireContext(), R.color.white)
-        lineDataSet1.setCircleColor(R.color.design_default_color_primary)
-        lineDataSet1.lineWidth = 4f
-        lineDataSet1.highLightColor = ContextCompat.getColor(requireContext(), R.color.teal_200)
-        lineDataSet1.setDrawHorizontalHighlightIndicator(false)
-        lineDataSet1.setDrawValues(false)
-
-        val dates = timelineDateReversed.map { it.date }
-
-        chart.apply {
-            data = lineData
-            description.isEnabled = false
-            legend.isEnabled = false
-            xAxis.valueFormatter = DateXAxisValueFormatter(dates)
-            xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
-            xAxis.setDrawGridLines(false)
-            axisLeft.isEnabled = false
-            axisRight.yOffset = -6f
-            axisRight.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
-            //xAxis.granularity = 10f
-            isDoubleTapToZoomEnabled = false
-
-            setDrawGridBackground(false)
-            //setVisibleXRangeMinimum(6f)
-
-        }
-
-        val mv = CustomMarkerView(requireContext(), R.layout.custom_marker_view)
-        mv.chartView = chart
-        chart.marker = mv
-
-        chart.invalidate()
-    }
-
-    private fun addIdToTimelineData(timelineData: List<CountryData.TimelineData>) {
-        for ((i, data) in timelineData.withIndex()) {
-            data.id = i
-        }
-    }
-
 
 }
